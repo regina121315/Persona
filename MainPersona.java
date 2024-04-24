c38c0d8f37f84812609fbbc095f4562c57d29339
Regina Diaz <regval3307@gmail.com>
2024-04-24 16:50:48 -0600

    version 1

---
+++ MainPersona.java
@@ -0,0 +1,13 @@
+/**
+ * MainPersona
+ */
+public class MainPersona {
+
+ public static void main(String[] args) {
+    Persona lechuga =new Persona();
+    lechuga.edad=19;
+    lechuga.mostrarEdad();
+    
+
+ }
+}
---
+++ Persona.java
@@ -0,0 +1,8 @@
+class Persona {
+    String nombre,genero;
+    int edad ;
+    public void mostrarEdad(){
+        System.out.println("tengo: "+ this.edad +" años");
+
+    }
+}
