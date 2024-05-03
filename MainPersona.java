03b7f9b0583845b759d374515677d14789166893
Regina Diaz <regval3307@gmail.com>
2024-05-03 00:15:29 -0600

    version 2

--- MainPersona.java
+++ MainPersona.java
@@ -1,13 +1,30 @@
-/**
- * MainPersona
- */
-public class MainPersona {
 
- public static void main(String[] args) {
-    Persona lechuga =new Persona();
-    lechuga.edad=19;
-    lechuga.mostrarEdad();
-    
+import java.util.Scanner;
+class MainPersona {
+    public static void main (String [] args){
+        String nombre,apellido;
+        int edad;
+        double altura;
+        Scanner op = new Scanner (System.in);
+        Persona paco = new Persona ();
+        System.out.println ("¿Cual es tu nombre?");
+        nombre=op.nextLine();
+        paco.setNombre(nombre);
+        System.out.println ("¿Cual es tu apellido?");
+        apellido=op.nextLine();
+        paco.setApellido(apellido);
+        System.out.println ("¿Cual es tu edad?");
+        edad=op.nextInt();
+        paco.setEdad(edad);
+        System.out.println ("¿Cual es tu altura?");
+        altura=op.nextDouble();
+        paco.setAltura(altura);
+        
+        System.out.println(paco);
 
- }
-}
+
+        
+    }
+
+   
+    }
--- Persona.java
+++ Persona.java
@@ -1,8 +1,58 @@
 class Persona {
-    String nombre,genero;
-    int edad ;
-    public void mostrarEdad(){
-        System.out.println("tengo: "+ this.edad +" años");
+    private String nombre,apellido;
+    private int edad;
+    private double altura;
 
-    }
+        public void mostrarNombre(){
+          System.out.println("Hola soy" + this.nombre);
+        }
+        public String getNombre() {
+            return nombre;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getApellido() {
+            return apellido;
+        }
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+        public int getEdad() {
+            return edad;
+        }
+        public void setEdad(int edad) {
+            if (edad>0) {
+                this.edad = edad;
+            } else {
+                System.out.println("edad invalidad");
+            }
+           
+        }
+        public double getAltura() {
+            return altura;
+        }
+        public void setAltura(double altura) {
+            if (altura>0) {
+                this.altura = altura;  
+            } else {
+                System.out.println("altura invalidad");
+            }
+            
+        }
+        @Override
+        public String toString() {
+            return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura
+                    + "]";
+        }
+        public void mostrarApellido(){
+            System.out.println("Mi apellido es" + this.apellido);
+        }
+        public void mostrarEdad(){
+            System.out.println ("Mi edad es:" + this.edad);
+        }
+        public void mostrarAltura(){
+            System.out.println ("Mi altura es:" + this.altura);
+    
+}
 }
